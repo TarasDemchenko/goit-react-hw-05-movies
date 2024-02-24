e1e795e519669145ce7c8ffd4c3442cab2ed82a3
TarasDemchenko <tarasdemchenko1993@gmail.com>
2024-02-24 19:51:42 +0100

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,19 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 export const App = () => {
+  const [state, setState] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+  'https://api.themoviedb.org/3/configuration/ac2a6bf47776780ec3fc263c9f74d565'
+      )
+      .then(response => {
+        setState(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
   return <></>;
 };
